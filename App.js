3f593e77d850fb51a0bbd17ae3970933b8428b85
Your Name <you@example.com>
2023-05-27 01:43:00 +0200

    DC

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#1D0F4A',
+      }}>
+      <WebView
+        style={styles.container}
+        source={{ uri: 'https://precio-justo-v2.vercel.app/' }}
+      />
+      <StatusBar style="light" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
